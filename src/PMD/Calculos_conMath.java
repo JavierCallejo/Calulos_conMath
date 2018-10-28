068d590fa2c0dd795af0c12f7d092cec050b724b
javiercallejo <javiercallejo@192.168.1.38>
2018-10-28 20:45:06 +0100

    Version 1

---
+++ src/PMD/Calculos_conMath.java
@@ -0,0 +1,16 @@
+package  PMD; 
+public class Calculos_conMath {
+
+	public static void main(String[] args) {
+		
+		int  base=5;
+		
+		 int resultados = Math.abs(base);
+		
+		System.out.println(resultados);
+
+	}
+
+}
+
+
